e061dc9bd231d7df79025054b438704d33a49d4e
Ian <haskel.list@yandex.ru>
2025-10-14 13:43:24 +0300

    extensive documentation

--- conf.py
+++ conf.py
@@ -1,28 +1,44 @@
 '''
-This module contains constants, representing
-constraints to prevent possible crushes due to
-limitations of RPI.
-Also this module is place where should be defined
-state that will not be changing for all time of bot
-execution, e.g. constants.
-'''
+Module contains mostly immutable state, beside the only mutable flag that
+represents receiving signal to shutdown. This flag is placed here to prevent
+cyclic import.
 
-MAX_RESPONSE_SIZE = 5 * 1024 * 1024  # 5MB limit for HTML responses
-MIN_DISK_SPACE_MB = 10  # Minimum free disk space required
+To represent existing state there are tags:
+- RaspberryPI constraints
+  Getting out of them leads to system crush
+
+- Destinations
+  Page URL, where we scrape data from
+  Path, where subscription data is stored
+
+- Mutable state
+  Whatever like flag that represents shutdown signal
+
+- CLI argument processing
+  Since all described above values can be provided from CLI, then
+  defined here variables should be default values for what can be defined
+  by user using CLI arguments.
+
+'''
+import os
+import argparse
+
+## RaspberryPI constraints
+MAX_RESPONSE_SIZE = 5 * 1024 * 1024    # 5MB limit for HTML responses
+MIN_DISK_SPACE_MB = 10                 # Minimum free disk space required
 STATE_FILE_MAX_SIZE = 1 * 1024 * 1024  # 1MB max for state files
 
-#TODO: add an ability to pass constans defined above as cli arguments. Keep defined values as default
+##Mutable state
+_shutdown_requested = False # Global flag for graceful shutdown
 
-# Global flag for graceful shutdown
-_shutdown_requested = False
 
-#base url that is used to retrieve information about QA
+##Destinations
+#base url that is used to retrieve information about QAn
 AVITO_URL="https://career.avito.com/vacancies/razrabotka/?q=&action=filter&direction=razrabotka&tags%5B%5D=s26502"
-
-import os
 SUBSCRIPTIONS_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), "bot_subscriptions.json")
 
-import argparse
+## CLI arguments processing
+#TODO: add an ability to pass constans defined above as cli arguments. Keep defined values as default
 _parser = argparse.ArgumentParser(description="Мониторинг QA-вакансий Avito Career")
 _parser.add_argument("--url", default=AVITO_URL, help="URL страницы вакансий")
 _parser.add_argument(
